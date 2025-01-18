9e70cd24e35a4b4c927ab84c783980e3359336ef
Jared Tate <13957390+JaredTate@users.noreply.github.com>
2025-01-18 15:35:49 -0700

    More Style Changes

--- client/src/views/navbar.js
+++ client/src/views/navbar.js
@@ -2,9 +2,12 @@ import Snabbdom from 'snabbdom-pragma'
 import menu from './navbar-menu'
 
 export default S =>
-
   <nav className="container nav-container">
-      <a className="navbar-brand" href="."></a>
-      { menu(S) }
+    <a className="navbar-brand" href="."></a>
+    <div className="site-title">
+      <h1>DigiExplorer</h1>
+      <h2>DigiByte Block Explorer</h2>
+    </div>
+    { menu(S) }
   </nav>
 
